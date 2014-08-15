3bf49bbeb6644ede6b311a91bec4e671e1bec171
som <sompathaka@gmail.com>
2014-08-15 18:39:39 +0530

    Update handler.js

--- handler.js
+++ handler.js
@@ -10,15 +10,7 @@ Library used:
 
 
 */ 
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-51835123-1']);
-_gaq.push(['_trackPageview']);
 
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
 
 var follow_item = $('a[class^="follow_button"]');
 var follow_text = follow_item.text();
